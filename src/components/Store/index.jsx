6262bee48f48a0068e6ddb6b76cfb5215c77646a
bassijtsma <btsijtsma@gmail.com>
2016-11-22 22:18:18 +0100

    setup message reducer

--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -1,5 +1,10 @@
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from '../Reducers'
 
+let store = createStore();
 
+store.subscribe(() => console.log(store.getState()))
 
 
 // https://github.com/cloudmu/react-redux-starter-kit/blob/master/src/store/configureStore.js
