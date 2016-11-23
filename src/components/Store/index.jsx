43b9c7d0dc4391765b7edc5cb1544d6a49a6af94
bassijtsma <btsijtsma@gmail.com>
2016-11-23 20:01:55 +0100

    added basic data model for messages

--- src/components/Store/index.jsx
+++ src/components/Store/index.jsx
@@ -10,3 +10,48 @@ store.subscribe(() => console.log(store.getState()))
 // https://github.com/cloudmu/react-redux-starter-kit/blob/master/src/store/configureStore.js
 
 //  https://github.com/tj/frontend-boilerplate/blob/master/client/store/index.js
+
+// design the state:
+
+{
+  userid: '123abc',
+  username: 'hank',
+  messages: [
+    {
+      nr: 1,
+      text: 'blabla numero 1',
+      sender: user,
+      timestamp: '10:30',
+      isSending: false
+    },
+    {
+      nr: 2,
+      text: 'certainly nr 2',
+      sender: bot,
+      timestamp: '10:31',
+      isSending: false
+    },
+    {
+      nr: 3,
+      text: 'alright nr 3',
+      sender: user,
+      timestamp: '10:32',
+      isSending: false
+    },
+    {
+      nr: 4,
+      text: 'ok ok ok nr 4',
+      sender: bot,
+      timestamp: '10:33',
+      isSending: false
+    },
+    ,
+    {
+      nr: 5,
+      text: 'sending ... 5',
+      sender: bot,
+      timestamp: '10:34',
+      isSending: true
+    }
+  ]
+}
