86b5b2509bc7beb8874a3524709507d7145435fb
NL\SSman <ssman@deloitte.nl>
2016-12-06 22:26:31 +0100

    Stylized message options + text-open arrow

--- src/components/MessageItem/MessageItem.jsx
+++ src/components/MessageItem/MessageItem.jsx
@@ -5,15 +5,15 @@ const MessageItem = ({sender, messagetext, msgoptions}) => (
 	<div className={sender}>
 		<div className="message-container">
 			<div className="message-body">
-				<div className="message">
+				<div className={ "message" + (msgoptions ? " message-with-options" : "") }>
 					<p>{messagetext}</p>
 				</div>
 				{msgoptions ? <MessageOptions msgoptions={msgoptions ? msgoptions : [1, 2]}/>
 				: ''}
 			</div>
 		</div>
-		<div className="message-footer">
-			<img src={sender == 'person' ? "img/textright.png" : "img/textleft.png" } className="message-arrow"/>
+		<div className={ "message-footer" + (msgoptions ? " open" : "") }>
+			<img src={sender == 'person' ? "img/textright.png" : msgoptions ? "img/text-open.png" : "img/textleft.png" } className="message-arrow"/>
 			<img src={sender == 'person' ? "img/avatar-small.png" : "img/bot icon.png" } className="avatar-img"/>
 		</div>
 	</div>
