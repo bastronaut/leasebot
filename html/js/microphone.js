832bb7a4cdf76a96b46d0ef667bca066b032cbab
bassijtsma <btsijtsma@gmail.com>
2017-01-05 23:13:27 +0100

    experiment with recorder

--- html/js/microphone.js
+++ html/js/microphone.js
@@ -1,33 +1,44 @@
-var recorder = document.getElementById('recorder');
-var player = document.getElementById('player');
+// var recorder = document.getElementById('recorder');
+// var player = document.getElementById('player');
+//
+// recorder.addEventListener("change", function(e) {
+// 	var file = e.target.files[0];
+// 	// Do something with the audio file.
+// 	player.src = URL.createObjectURL(file);
+// });
 
-recorder.addEventListener("change", function(e) {
-	var file = e.target.files[0];
-	// Do something with the audio file.
-	player.src = URL.createObjectURL(file);
+//
+//
+//
+// var playertwo = document.getElementById('player');
+//
+// var handleSuccess = function(stream) {
+// 	if (window.URL) {
+// 		playertwo.src = window.URL.createObjectURL(stream);
+// 	} else {
+// 		playertwo.src = stream;
+// 	}
+// };
+//
+// navigator.mediaDevices.getUserMedia({
+// 		audio: true,
+// 		video: false
+// 	})
+// 	.then(handleSuccess)
+
+
+navigator.permissions.query({name:'microphone'}).then(function(result) {
+  if (result.state == 'granted') {
+      console.log("its granted!");
+  } else if (result.state == 'prompt') {
+      console.log("its prompt!!");
+  } else if (result.state == 'denied') {
+    console.log("its denied!");
+  }
+  result.onchange = function() {
+
+  };
 });
-
-
-
-
-var playertwo = document.getElementById('player');
-
-var handleSuccess = function(stream) {
-	if (window.URL) {
-		playertwo.src = window.URL.createObjectURL(stream);
-	} else {
-		playertwo.src = stream;
-	}
-};
-
-navigator.mediaDevices.getUserMedia({
-		audio: true,
-		video: false
-	})
-	.then(handleSuccess)
-
-
-
 
 
 
@@ -48,6 +59,7 @@ var handleSuccess = function(stream) {
 	const mediaRecorder = new MediaRecorder(stream, options);
 
 	mediaRecorder.addEventListener('dataavailable', function(e) {
+    console.log('data available!');
 		if (e.data.size > 0) {
 			recordedChunks.push(e.data);
 		}
